c92c92b0ccd18f9ecccd9ad43704fc0a8bc32ed8
techEdge3030 <atlas3030wei@gmail.com>
2025-08-19 22:37:38 +0700

    initial commit

---
+++ src/layout/Banners/StatusBanner.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { email } from '$data/api/settings';
+	import Banner from '$layout/Banners/Banner.svelte';
+
+	export let statusUrl: string | null = null;
+
+	const handleClick = () => statusUrl && goto(statusUrl);
+</script>
+
+<Banner
+	title="API service is currently down"
+	description="Some features of the site might be impacted. If this issue persists, reach out to mailto:{email()}"
+	buttonText={statusUrl ? 'View status' : undefined}
+	buttonOnClick={statusUrl ? handleClick : undefined}
+	level="caution"
+	permanent
+/>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,77 @@
+<script lang="ts" context="module">
+	import { writable } from 'svelte/store';
+	// There might be a better place to put this, but I am not entirely sure...
+	export const isRestoring = writable(false);
+</script>
+
+<script lang="ts">
+	import '../app.scss';
+	import { derived } from 'svelte/store';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+
+	import { QueryClient } from '@tanstack/query-core';
+	import { persistQueryClient } from '@tanstack/query-persist-client-core';
+	import { QueryClientProvider } from '@tanstack/svelte-query';
+	import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
+	import { DateTriggerEventHandler } from 'datetrigger';
+
+	import NavHost from '$layout/Navbar/NavHost.svelte';
+	import Spinner from '$lib/components/Spinner.svelte';
+	import ConsentDialog from '$layout/Dialogs/ConsentDialog.svelte';
+	import { staleTime } from '$data/api';
+	import RouterEvents from '$data/RouterEvents';
+	import { events as themeEvents } from '$util/themeEvents';
+
+	import FooterHost from '$layout/Footer/FooterHost.svelte';
+
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser,
+				cacheTime: staleTime
+			}
+		}
+	});
+
+	// Just like the set/clearInterval example found here: https://svelte.dev/docs#run-time-svelte-store-derived
+	const show_loading_animation = derived(
+		RouterEvents,
+		($event, set) => {
+			if ($event.navigating) {
+				// Wait 250 ms before showing the animation.
+				const timeout = setTimeout(() => set(true), 250);
+				return () => clearTimeout(timeout);
+			} else {
+				set(false);
+			}
+		},
+		false
+	);
+
+	onMount(() => {
+		new DateTriggerEventHandler(themeEvents);
+
+		isRestoring.set(true);
+		const [unsubscribe, promise] = persistQueryClient({
+			queryClient,
+			persister: createSyncStoragePersister({ storage: localStorage })
+		});
+		promise.then(() => isRestoring.set(false));
+		return unsubscribe;
+	});
+</script>
+
+<ConsentDialog />
+
+<QueryClientProvider client={queryClient}>
+	<NavHost />
+	<div id="skiptab">
+		{#if $show_loading_animation}
+			<Spinner />
+		{:else}
+			<slot />
+		{/if}
+	</div>
+	<FooterHost />
+</QueryClientProvider>
